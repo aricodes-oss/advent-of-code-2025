3214f022233ddf15fc991fe1ab8e3229ff10bde7
Aria Taylor <ari@aricodes.net>
2025-12-08 21:30:37 -0800

    Slightly less naive part2 attempt

--- 07/part2.py
+++ 07/part2.py
@@ -8,19 +8,24 @@ SPLITTER = "^"
 START = "S"
 EMPTY = "."
 
+paths = set()
 
-def _fall(pos: int, line_no: int, lines: list[str]) -> int:
-    # Base case
-    if line_no == len(lines):
-        return 1
+
+def _fall(trace: list[str], line_no: int, lines: list[str]) -> None:
+    # Base case -
+    if line_no == len(lines) - 1:
+        paths.add(" ".join(map(str, trace)))
+        return
 
     line = lines[line_no]
-    ch = line[pos]
+    pos = trace[-1]
+    ch = line[trace[-1]]
 
     if ch == SPLITTER:
-        return _fall(pos - 1, line_no + 1, lines) + _fall(pos + 1, line_no + 1, lines)
-
-    return _fall(pos, line_no + 1, lines)
+        for origin in [pos - 1, pos + 1]:
+            _fall(trace + [origin], line_no + 1, lines)
+    elif ch in [START, EMPTY]:
+        _fall(trace, line_no + 1, lines)
 
 
 def main(args) -> int | None:
@@ -29,7 +34,8 @@ def main(args) -> int | None:
         lines = f.read().rstrip().split("\n")
 
     start = lines[0].index(START)
-    print(_fall(start, 1, lines))
+    _fall([start], 0, lines)
+    print(len(list(paths)))
 
 
 if __name__ == "__main__":
